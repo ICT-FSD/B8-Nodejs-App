71526d528129ac832d8647b98cadaced9ba7efd9
sony-joy <sony.joy@hpe.com>
2023-10-03 21:59:24 +0530

    tutorial app folder structure and initial commit

--- app/models/index.js
+++ app/models/index.js
@@ -9,20 +9,3 @@ db.url = dbConfig.url;
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
-
-const app = express();
-app.use(express.json());
-
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
